37357ce569c825424930eacfb5771d743cefbb67
hafez-elashry <hafez.elashry@gmail.com>
2021-06-28 12:25:34 +0200

    Update SearchPage.js

--- src/component/SearchPage.js
+++ src/component/SearchPage.js
@@ -21,7 +21,7 @@ class SearchPage extends React.Component {
               <li key={book.id}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail ? book.imageLinks.smallThumbnail : ""})`}}></div>
                   <div className="book-shelf-changer">
                     <select
                       value={book.shelf? book.shelf : "none"}
